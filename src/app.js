806e5b1b2ffad94582f43d14f9b72eb85650ee7e
Akmall-rey <raihanakmal354@gmail.com>
2025-04-27 13:45:17 +0700

    feat: update history attendance modal

--- src/app.js
+++ src/app.js
@@ -5,10 +5,13 @@ import helmet from "helmet";
 import morgan from "morgan";
 import dotenv from "dotenv";
 import path from "path";
-import { fileURLToPath } from "url";
+import {
+	fileURLToPath
+} from "url";
 
 // get __dirname in ES Modules
-const __filename = fileURLToPath(import.meta.url);
+const __filename = fileURLToPath(
+	import.meta.url);
 const __dirname = path.dirname(__filename);
 
 // Load environment variables
@@ -37,9 +40,13 @@ app.set("views", path.join(__dirname, "../views"));
 
 // Middleware
 app.use(cors());
-app.use(helmet({ contentSecurityPolicy: false }));
+app.use(helmet({
+	contentSecurityPolicy: false
+}));
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({
+	extended: true
+}));
 
 // Serve static files from "public"
 app.use(express.static(path.join(__dirname, "../public")));
@@ -67,6 +74,14 @@ app.use("/addemployee", addEmployeeRoutes);
 app.get("/attendance", (req, res) => {
 	res.render("attendance");
 });
+
+app.get('/login', (req, res) => {
+	res.render('login', {
+		title: 'Login | HR System',
+		showSidebar: false
+	});
+});
+
 
 app.get("/allrequests", (req, res) => {
 	res.render("allrequests");
@@ -98,13 +113,18 @@ app.get("/profile", (req, res) => {
 
 // 404 handler
 app.use((req, res, next) => {
-	res.status(404).json({ message: "Route not found" });
+	res.status(404).json({
+		message: "Route not found"
+	});
 });
 
 // Error handler
 app.use((err, req, res, next) => {
 	console.error(err.stack);
-	res.status(500).json({ message: "Server error", error: err.message });
+	res.status(500).json({
+		message: "Server error",
+		error: err.message
+	});
 });
 
 export default app;
