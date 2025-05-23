ac5899e57f1c0e332c80e510e2fc3944f7328125
Rakhaditowb <rakhaditobassam@gmail.com>
2025-05-23 10:04:59 +0700

    chore: editing db.js for deploy

--- src/db.js
+++ src/db.js
@@ -1,10 +1,13 @@
 import mysql from "mysql2/promise";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const db = await mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "",
-  database: "hrsystem-api-db"
+	host: process.env.DB_HOST || "localhost",
+	user: process.env.DB_USER || "root",
+	password: process.env.DB_PASSWORD || "",
+	database: process.env.DB_NAME || "hrsystem-api-db",
 });
 
 console.log("MySQL connected.");
