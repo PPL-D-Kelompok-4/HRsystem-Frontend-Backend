4f480a136ae49c6f172227d26b87cd887e2bd0c0
Rakhaditowb <rakhaditobassam@gmail.com>
2025-06-22 21:35:55 +0700

    fix: fixing db error in dashboard when deploying

--- src/db.js
+++ src/db.js
@@ -3,14 +3,16 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-const db = await mysql.createConnection({
+// Create a connection pool
+const pool = mysql.createPool({
 	host: process.env.DB_HOST,
 	user: process.env.DB_USER,
 	password: process.env.DB_PASSWORD,
 	database: process.env.DB_NAME,
 	port: process.env.DB_PORT,
+	waitForConnections: true,
+	connectionLimit: 10,
+	queueLimit: 0,
 });
 
-console.log("MySQL connected.");
-
-export default db;
+export default pool;
